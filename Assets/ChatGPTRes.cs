4af08ce00125ebcc4e0aed9ce204342a08de88c1
Jack Yang <jackyangzzh@gmail.com>
2023-02-13 18:18:45 -0800

    Initial progress

---
+++ Assets/ChatGPTRes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChatGPTWrapper {
+    [Serializable]
+    public class ChatGPTRes
+    {
+        public string id;
+        public List<Choices> choices;
+    }
+}
