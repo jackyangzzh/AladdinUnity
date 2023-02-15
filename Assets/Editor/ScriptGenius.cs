6b36e1ca05aab48389803ba9e1630357e254cc5c
Jack Yang <jackyangzzh@gmail.com>
2023-02-14 19:19:55 -0800

    Script genius editor initate

---
+++ Assets/Editor/ScriptGenius.cs
@@ -0,0 +1,67 @@
+using Codice.Client.Common.GameUI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ScriptGenius : EditorWindow
+{
+    [MenuItem("Script Genius/Script Genius")]
+    public static void ShowWindow()
+    {
+        var window = GetWindow<ScriptGenius>();
+        window.titleContent = new GUIContent("Script Genius");
+        window.Show();
+    }
+
+    private ScriptGeniusElement baseElement;
+
+    private void OnEnable()
+    {
+        baseElement = new ScriptGeniusElement(this);
+        rootVisualElement.Add(baseElement);
+    }
+}
+
+public class ScriptGeniusElement : VisualElement
+{
+    private const string CSharpText = "C#";
+    private const string ShaderText = "Shader";
+
+    public ScriptGeniusElement(EditorWindow window)
+    {
+        TextField scriptNameField = new()
+        {
+            label = "Script Name",
+        };
+        Add(scriptNameField);
+
+        DropdownField scriptTypeField = new()
+        {
+            label = "Script Type",
+            choices = { CSharpText, ShaderText },
+            index = 0,
+        };
+        Add(scriptTypeField);
+
+        TextField scriptPromptField = new()
+        {
+            label = "Script Prompt"
+        };
+        Add(scriptPromptField);
+
+        Button generateScriptButton = new(GenerateScript)
+        {
+            text = "Generate Script"
+        };
+        Add(generateScriptButton);
+
+        
+        void GenerateScript()
+        {
+            Debug.Log(scriptNameField.text);
+        }
+    }
+
+}
