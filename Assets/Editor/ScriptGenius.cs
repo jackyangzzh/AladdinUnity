65e93cf8d02533ae6c45a39e31a461e496052a4c
Jack Yang <jackyangzzh@gmail.com>
2023-02-24 07:23:32 -0800

    Save progress of refactor

--- Assets/Editor/ScriptGenius.cs
+++ Assets/Editor/ScriptGenius.cs
@@ -1,74 +1,135 @@
-using ChatGPTWrapper;
 using UnityEditor;
-using UnityEditor.UIElements;
-using UnityEngine;
-using UnityEngine.UIElements;
-
 
 public class ScriptGenius : EditorWindow
 {
-    [MenuItem("Script Genius/Script Genius")]
+    //private string scriptName;
+
+    [MenuItem("Window/ScriptGenius")]
     public static void ShowWindow()
     {
-        var window = GetWindow<ScriptGenius>();
-        window.titleContent = new GUIContent("Script Genius");
-        window.Show();
+        EditorWindow.GetWindow(typeof(ScriptGenius));
     }
 
-    private ScriptGeniusElement baseElement;
+    //[MenuItem("Script Genius/Script Genius")]
+    //public static void ShowWindow()
+    //{
+    //    GetWindow<ScriptGenius>();
+    //    //var window = GetWindow<ScriptGenius>();
+    //    //window.titleContent = new GUIContent("Script Genius");
+    //    //window.Show();
+    //}
+
+    //private string scriptPrompt;
+    //private ChatGPTSetting chatGPTSetting;
+    //private Button generateScriptButton;
+
+
+    //private ScriptGeniusElement baseElement;
 
     private void OnEnable()
     {
-        baseElement = new ScriptGeniusElement(this);
-        rootVisualElement.Add(baseElement);
+        //baseElement = new ScriptGeniusElement(this);
+        //rootVisualElement.Add(baseElement);
+        //chatGPTSetting =  Resources.Load("DefaultChatGPTSetting") as ChatGPTSetting;
     }
-}
 
-public class ScriptGeniusElement : VisualElement
-{
-    public ScriptGeniusElement(EditorWindow window)
+    void OnGUI()
     {
-        TextField scriptNameField = new()
-        {
-            label = "Script Name",
-        };
-        Add(scriptNameField);
+        //scriptName = EditorGUILayout.TextField("Script Name", scriptName);
+        //chatGPTSetting = EditorGUILayout.ObjectField("Setting", chatGPTSetting, typeof(ChatGPTSetting)) as ChatGPTSetting;
+        //scriptPrompt = EditorGUILayout.TextArea("Script Prompt", scriptPrompt);
 
-        DropdownField scriptTypeField = new()
-        {
-            label = "Script Type",
-            choices = { ScriptGeniusUtil.CSharpText, ScriptGeniusUtil.ShaderText },
-            index = 0,
-        };
-        Add(scriptTypeField);
-
-        ObjectField settingField = new()
-        {
-            label = "Setting",
-            value = Resources.Load("DefaultChatGPTSetting"),
-            objectType = typeof(ChatGPTSetting),
-
-        };
-        Add(settingField);
-
-        TextField scriptPromptField = new()
-        {
-            label = "Script Prompt"
-        };
-        Add(scriptPromptField);
-
-        Button generateScriptButton = new(GenerateScript)
-        {
-            text = "Generate Script"
-        };
-        Add(generateScriptButton);
-
-
-        void GenerateScript()
-        {
-            ChatGPTHelper chatGPT = new(scriptNameField.value, scriptTypeField.value);
-            chatGPT.SendToChatGPT(scriptPromptField.text, settingField.value as ChatGPTSetting);
-        }
+        //if (GUILayout.Button("Generate Script"))
+        //{
+        //    GenerateScript();
+        //}
     }
 
+    //void GenerateScript()
+    //{
+    //    _ = EditorCoroutineUtility.StartCoroutine(GenerateScriptAsync(), this);
+    //}
+
+    //IEnumerator GenerateScriptAsync()
+    //{
+    //    ChatGPTHelper chatGPT = new(scriptName, ScriptGeniusUtil.CSharpExtension);
+    //    chatGPT.SendToChatGPT(scriptPrompt, chatGPTSetting);
+
+    //    generateScriptButton.text = "Generating script...";
+    //    generateScriptButton.SetEnabled(false);
+
+    //    while (!chatGPT.IsCompleted)
+    //    {
+    //        yield return null;
+    //    }
+
+    //    generateScriptButton.text = "Generate Script";
+    //    generateScriptButton.SetEnabled(true);
+    //}
 }
+
+//public class ScriptGeniusElement : VisualElement
+//{
+//    public ScriptGeniusElement(EditorWindow window)
+//    {
+//        TextField scriptNameField = new()
+//        {
+//            label = "Script Name",
+//        };
+//        Add(scriptNameField);
+
+//        DropdownField scriptTypeField = new()
+//        {
+//            label = "Script Type",
+//            choices = { ScriptGeniusUtil.CSharpText, ScriptGeniusUtil.ShaderText },
+//            index = 0,
+//        };
+//        Add(scriptTypeField);
+
+//        ObjectField settingField = new()
+//        {
+//            label = "Setting",
+//            value = Resources.Load("DefaultChatGPTSetting"),
+//            objectType = typeof(ChatGPTSetting),
+
+//        };
+//        Add(settingField);
+
+//        TextField scriptPromptField = new()
+//        {
+//            label = "Script Prompt"
+//        };
+//        Add(scriptPromptField);
+
+//        Action action = () => GenerateScript();
+
+//        Button generateScriptButton = new(action)
+//        {
+//            text = "Generate Script"
+            
+//        };
+//        Add(generateScriptButton);
+
+//        void GenerateScript()
+//        {
+//            //_ = EditorCoroutineUtility.StartCoroutine(GenerateScriptAsync(), this);
+//        }
+
+//        IEnumerator GenerateScriptAsync()
+//        {
+//            ChatGPTHelper chatGPT = new(scriptNameField.value, scriptTypeField.value);
+//            chatGPT.SendToChatGPT(scriptPromptField.text, settingField.value as ChatGPTSetting);
+
+//            generateScriptButton.text = "Generating script...";
+//            generateScriptButton.SetEnabled(false);
+
+//            while (!chatGPT.IsCompleted)
+//            {
+//                yield return null;
+//            }
+
+//            generateScriptButton.text = "Generate Script";
+//            generateScriptButton.SetEnabled(true);
+//        }
+//    }
+//}
